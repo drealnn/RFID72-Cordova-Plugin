e227038897c29a2a336982943d553f4b5c9ea879
Daniel <drealnn@gmail.com>
2020-01-22 14:28:38 -0500

    Removing key events from General class

--- src/android/General.java
+++ src/android/General.java
@@ -1,4 +1,4 @@
-package com.ssg.cordova;
+package com.rfid72.app;
 
 import android.media.AudioManager;
 import android.media.SoundPool;
@@ -22,7 +22,7 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 
-public class Keyboard extends CordovaPlugin {
+public class General extends CordovaPlugin implements View.OnKeyListener {
 
 
     private CallbackContext keyup_callback = null;
@@ -40,13 +40,12 @@ public class Keyboard extends CordovaPlugin {
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
         result.setKeepCallback(true);
+
         // Events //
-        if(action.equalsIgnoreCase("register_keyDown")){
-            this.keydown_callback = callbackContext;
-            return true;
-        }
-        else if(action.equalsIgnoreCase("register_keyUp")){
-            this.keyup_callback = callbackContext;
+
+        if(action.equalsIgnoreCase("playSound"))
+        {
+            this.playSound(args.getString(0), callbackContext);
             return true;
         }
         return false;
@@ -55,99 +54,48 @@ public class Keyboard extends CordovaPlugin {
     @Override
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
-        Log.i(TAG, "Keyboard Capture Plugin Initialized");
+
+        Log.i(TAG, "RFID72 general Initialized");
         ctx = cordova.getActivity().getApplicationContext();
-		View.OnKeyListener listener = new View.OnKeyListener(){
-			@Override
-			public boolean onKey(View view, int keyCode, KeyEvent event){
-				//boolean val = super.onKey(view, keyCode, event);
-				Log.e(TAG, ""+keyCode);
-				return doKey(view, keyCode, event);
-			}
-		}
-		if (webView.getView() instanceof CustomCordovaSystemWebView){
-			webView.getView().setCustomOnKeyListener(listener);
-		} else {
-			webView.getView().setOnKeyListener(listener);
-		}
+        //final CordovaWebView myWebView = webView;
+        /*cordova.getActivity().runOnUiThread(new Runnable() {
+            public void run() {
+                myWebView.getView().setOnKeyListener(
+                        new View.OnKeyListener(){
+                            @Override
+                            public boolean onKey(View view, int keyCode, KeyEvent event){
+                                //boolean val = super.onKey(view, keyCode, event);
+                                Log.e(TAG, ""+keyCode);
+                                return doKey(view, keyCode, event);
+                            }
+                        }
+                );
+            }
+        });*/
+
         this.currentView = webView.getView();
+
     }
 
 
+    private void playSound(String soundName, CallbackContext callbackContext)
+    {
+        switch(soundName){
+            case("success"):
+                SoundLoader.getInstance(ctx).playSuccess();
+                break;
+            case("fail"):
+                SoundLoader.getInstance(ctx).playFail();
+                break;
+            case("beep"):
+                SoundLoader.getInstance(ctx).playBeep();
+                break;
+            default:
+                callbackContext.error("Can't find provided sound for playSound");
+                return;
+                
 
-// ####### Key event functions
-
-    public boolean doKey(View v, int keyCode, KeyEvent event) {
-
-        Log.i(TAG, "triggering key event");
-        if (event.getAction() == KeyEvent.ACTION_UP) {
-            return KeyUp(keyCode, event);
         }
-        else if (event.getAction() == KeyEvent.ACTION_DOWN) {
-            return KeyDown(keyCode, event);
-        }
-        return false;
-    }
-
-    private boolean KeyDown(int keyCode, KeyEvent event){
-        //if(keydown_callback == null){
-        //  return true;
-        //}
-        //PluginResult result = new PluginResult(PluginResult.Status.OK, Integer.toString(keyCode));
-        //result.setKeepCallback(true);
-        //keydown_callback.sendPluginResult(result);
-
-        Log.e(TAG, "Key down in native");
-        if (this.keydown_callback == null)
-            return false;
-
-        try {
-
-            String str = String.format("{\'keyCode\': \'%s\', \'repeatCount\' : \'%s\' }", keyCode + "", event.getRepeatCount() + "");
-            PluginResult result = new PluginResult(PluginResult.Status.OK, new JSONObject(str));
-            result.setKeepCallback(true);
-            this.keydown_callback.sendPluginResult(result);
-            
-        } catch(Exception e)
-        {
-            e.printStackTrace();
-            PluginResult result = new PluginResult(PluginResult.Status.ERROR, "Error in handling key event");
-            result.setKeepCallback(true);
-            this.keydown_callback.sendPluginResult(result);
-            
-        }
-
-        return false;
-    }
-
-    private boolean KeyUp(int keyCode, KeyEvent event){
-        //if(keyup_callback == null){
-        //  return true;
-        //}
-        //PluginResult result = new PluginResult(PluginResult.Status.OK, Integer.toString(keyCode));
-        //result.setKeepCallback(true);
-        //keyup_callback.sendPluginResult(result);
-        if (this.keyup_callback == null)
-            return false;
-
-        try {
-
-            String str = String.format("{\'keyCode\': \'%s\', \'repeatCount\' : \'%s\' }", keyCode + "", event.getRepeatCount() + "");
-            PluginResult result = new PluginResult(PluginResult.Status.OK, new JSONObject(str));
-            result.setKeepCallback(true);
-            this.keyup_callback.sendPluginResult(result);
-            
-        } catch(Exception e)
-        {
-            e.printStackTrace();
-            PluginResult result = new PluginResult(PluginResult.Status.ERROR, "Error in handling key event");
-            result.setKeepCallback(true);
-            this.keyup_callback.sendPluginResult(result);
-            
-        }
-
-        return false;
-
+        callbackContext.success("Initiating sound");
     }
 }
-
